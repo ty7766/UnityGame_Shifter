cca375010c436deb54d050ed605689f8c8527179
hyojoon Lim <hyojoon42@naver.com>
2025-05-21 18:38:15 +0900

    feature : sunset일 때 블록 멈춤 추가

--- Assets/Scripts/MovingBlock.cs
+++ Assets/Scripts/MovingBlock.cs
@@ -2,21 +2,38 @@
 
 public class MovingBlock : MonoBehaviour
 {
+    [Header("블록 속성")]
     public float speed = 2.0f;          // 블록 이동 속도
     public float range = 4.0f;          // 블록 이동 범위
     public string direction = "right";  // 이동 방향
-
+    public bool isStopped = false;      // 블록 이동 유무
     private Vector3 defPos;             // 시작 위치
     private GameObject playerOnTop;     // 올라온 플레이어 참조
 
+    [Header("연결 스크립트")]
+    public ShiftController shiftController;     //배경 색상에 따른 블록 제어
+    
+    //초기화
     void Start()
     {
+        //시작위치 초기화
         defPos = transform.position;
+        //크기 초기화
         transform.localScale = new Vector3(0.8f, 0.8f, 1f); // 블록 크기 고정
     }
 
     void Update()
     {
+        //배경이 노을일 때 블록 정지
+        if (shiftController != null)
+        {
+            string back = shiftController.GetCurrentBackName();
+            isStopped = (back == "back_sunset");
+        }
+
+        if (isStopped)
+            return;
+
         // 이동 방향 반전
         if (transform.position.x < defPos.x - (range / 2))
         {
